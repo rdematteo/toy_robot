6035999e20c597670e80a8490448adbe9a1db6c5
Robert De Matteo <robertdematteo@hotmail.com>
2019-08-26 21:32:19 +1000

    toy robot able to turn left

--- lib/toy_robot/robot.rb
+++ lib/toy_robot/robot.rb
@@ -1,10 +1,12 @@
 module ToyRobot
   class Robot
-    attr_reader :east, :north
+    DIRECTIONS = ["NORTH", "EAST", "SOUTH", "WEST"]
+    attr_reader :east, :north, :direction
 
-    def initialize(east = 0, north = 0)
+    def initialize(east = 0, north = 0, direction = "NORTH")
       @east = east
       @north = north
+      @direction = direction
     end
 
     def move_east
@@ -21,6 +23,26 @@ module ToyRobot
 
     def move_south
       @north -= 1
+    end
+
+    def move
+      send("move_#{@direction.downcase}")
+      # case @direction
+      # when "NORTH" then move_north
+      # when "SOUTH" then move_south
+      # when "EAST" then move_east
+      # when "WEST" then move_west
+      # end
+    end
+
+    def turn_left
+      @direction = DIRECTIONS[DIRECTIONS.index(@direction)-1]
+      # @direction = case @direction
+      # when "NORTH" then "WEST"
+      # when "SOUTH" then "EAST"
+      # when "EAST" then "NORTH"
+      # when "WEST" then "SOUTH"
+      # end
     end
 
 
